bcb2b55814f6dffc9d837449fee5bd9604875cc2
MarioKapllani <32299334+MarioKapllani@users.noreply.github.com>
2019-02-18 23:42:22 +0100

    fixed login blank page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,27 +12,35 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/myJs.js') }}" defer></script>
-    @yield('dashscripts')
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link href="/css/app.css" rel="stylesheet" type="text/css">
-    <link href="/css/myStyle.css" rel="stylesheet" type="text/css">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @yield('dashstyles')
+
+    {{-- jquery print-page
+    <link rel="stylesheet" href="{{asset('css/bootstrap-theme.min.css')}}">
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script type="text/javascript" src="js/jquery.printPage.js"></script> --}}
+ <!-- Scripts -->
+ <script src="{{ asset('js/app.js') }}" defer></script>
+ <script src="{{ asset('js/myJs.js') }}" defer></script>
+ @yield('dashscripts')
+ <!-- Fonts -->
+ <link rel="dns-prefetch" href="//fonts.gstatic.com">
+ <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+ <link href="/css/app.css" rel="stylesheet" type="text/css">
+ <link href="/css/myStyle.css" rel="stylesheet" type="text/css">
+
+ <!-- Styles -->
+ <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+ @yield('dashstyles')
 
 </head>
-
 <body>
     <div id="page">
         @include('inc.navbar')
 
         <div class="container h-100">
-                @if(Auth::guest())
+               
                @yield('login')
-               @else
+              
             <div id="page-wrapper" style="min-height: calc(100vh - 55.5px) !important;">
                 <div class="row wrapper page-heading" style="margin-top: 55px">
                     <div class="col-lg-5 col-xs-12 col-md-5 no-padding">
@@ -41,17 +48,16 @@
                         @include('inc.messages')
                     </div>
                 </div>
-               
+                
                         <div class="row m-t-sm" id="dashboard-page">
                                 <div class="col-xs-12" id="dashboard">
-                @yield('content')
-                                </div>     
+                                       @yield('content')
+                      </div>     
                 </div>
             </div>
-            @endif
+            
         </div>
 
     </div>
 </body>
-
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,5 @@
-@extends('layouts.app')
-
+@extends('layouts.welcome')
 @section('login')
-@if(Auth::guest())
-@include('layouts.welcome')
         <div class="main-title text-center">
                 <img class="site-logo" src="images/BMS-LOGO.png">
             </div>
@@ -38,14 +35,30 @@
 
     </div>
 
-    @else
-
-    <h1>Welcome {{Auth::user()->name}}</h1>
-
-    @endif
-
-
     
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap-theme.min.css')}}">
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+
+    <script>
+        function getMessage() {
+           $.ajax({
+              type:'GET',
+              dataType: "json",
+              url:'http://127.0.0.1:8000/api/products/2',
+              success:function(data) {
+                 $("#msg").html(data.name);
+              }
+           });
+        }
+     </script>
+  </head>
+  
+     <div id = 'msg'>This message will be replaced using Ajax. 
+        Click the button to replace the message.</div>
+     <?php
+        echo "<form><button type='button' onClick='getMessage()'>Submit</button></form>";
+     ?>
 
 
 @endsection
