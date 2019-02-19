8817a549e075c37acb90ffa96cd62e1e1b6d8f6d
MarioKapllani <32299334+MarioKapllani@users.noreply.github.com>
2019-02-20 00:54:27 +0100

    ndryshim

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -8,7 +8,22 @@
 @section('content')
     @role('kamarier')
     <a href="orders/create" class="btn btn-primary">Krijo Porosi te re</a> <br> <br>
-    <table class="table table-striped">
+     <div class="table-wrapper">
+
+	<div class="table-title">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>Liste <b>Porosish</b></h2>
+                </div>
+                <div class="col-sm-6">
+                    <a href="#addProductModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i>
+                        <span>Shto te ri</span></a>
+                  
+                </div>
+            </div>
+        </div>
+		        <table class="table table-striped table-hover table-responsive">
+				<thead>
         <tr>
             <th>Order_ID</th>
             <th>Kamarier_id</th>
@@ -16,6 +31,8 @@
             <th>Koha e krijimit</th>
             <th>Shiko Detaje</th>
         </tr>
+		</thead>
+		<tbody>
         @foreach ($orders as $order)
             <tr>
                 <td>{{$order->id}}</td>
@@ -25,11 +42,27 @@
                 <td><a href="/orders/{{$order->id}}">Shiko Faturen {{$order->id}}</a></td>
             </tr>
         @endforeach
+		</tbody>
+		</table>
+		</div>
         @endrole
 
-        @role('menaxher|admin')
+        @role('menaxher|admin|ekonomist')
         <h1>Orders:</h1> <a href="orders/create" class="btn btn-primary">Krijo Porosi te re</a> <br> <br>
-    <table class="table table-striped">
+    	<div class="table-title">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>Liste <b>Porosish</b></h2>
+                </div>
+                <div class="col-sm-6">
+                    <a href="#addProductModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i>
+                        <span>Shto te ri</span></a>
+                  
+                </div>
+            </div>
+        </div>
+		        <table class="table table-striped table-hover table-responsive">
+				<thead>
         <tr>
             <th>Order_ID</th>
             <th>Kamarier_id</th>
@@ -37,8 +70,10 @@
             <th>Koha e krijimit</th>
             <th>Shiko Detaje</th>
         </tr>
+                </thead>
+                <tbody>
         @php
-           $orders = App\Order::orderBy('id','desc')->paginate(9);
+           $orders = App\Order::orderBy('id','desc')->paginate(2);
         @endphp
 
         @foreach ($orders as $order)
@@ -53,6 +88,8 @@
         @endforeach
         
             {{$orders->links()}}
-
+        </tbody>
+    </table>
+    </div>
         @endrole
 @endsection
