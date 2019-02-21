66794c7cea67604118b36cfd57620bb52f89ea9f
MarioKapllani <32299334+MarioKapllani@users.noreply.github.com>
2019-02-21 01:58:36 +0100

    rregullime listash

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -21,92 +21,98 @@
                     <span>Shto te ri</span></a>
             </div>
         </div>
-		        <table class="table table-striped table-hover table-responsive">
-				<thead>
-        <tr>
-            <th>Order_ID</th>
-            <th>Kamarier</th>
-            <th>Tavolina</th>
-            <th>Koha e krijimit</th>
-            <th>Shiko Detaje</th>
-        </tr>
-		</thead>
-		<tbody>
-        @foreach ($orders as $order)
-        @php $userfature=App\User::find($order->user_id)->name @endphp
-            <tr>
-                <td>{{$order->id}}</td>          
-                <td>{{$userfature}}</td>         
-                <td>{{$order->T_id}}</td>
-                <td>{{isset($order->created_at)?$order->created_at->format('H:i:s d/m/Y'):"no date"}}</td>
-                {{-- <td><a href="/orders/{{$order->id}}">Shiko Faturen {{$order->id}}</a></td> --}}
-                <td class="text-right">
-                    <button href="/orders/{{$order->id}}">
-                                <i class="material-icons eye" data-toggle="tooltip" title="Shiko Faturen">&#xE254;</i>
-                    </button>
-            </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+        <div class="table-responsive">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Order_ID</th>
+                        <th>Kamarier</th>
+                        <th>Tavolina</th>
+                        <th>Koha e krijimit</th>
+                        <th class="text-center">Detaje</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($orders as $order)
+                    @php $userfature=App\User::find($order->user_id)->name @endphp
+                    <tr>
+                        <td>{{$order->id}}</td>
+                        <td>{{$userfature}}</td>
+                        <td>{{$order->T_id}}</td>
+                        <td>{{isset($order->created_at)?$order->created_at->format('H:i:s d/m/Y'):"no date"}}</td>
+                        {{-- <td><a href="/orders/{{$order->id}}">Shiko Faturen {{$order->id}}</a></td> --}}
+                        <td class="text-center">
+                                <a href="/orders/{{$order->id}}">
+                                    <i class="fa fa-file edit" data-toggle="tooltip" title="Shiko Faturen"></i>
+                                </a>
+                            </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 @endrole
 
 @role('menaxher|admin|ekonomist')
 <a href="orders/create" class="btn btn-primary">Krijo Porosi te re</a> <br> <br>
 <div class="table-wrapper">
-<div class="table-title">
-    <div class="row">
-        <div class="col-sm-6">
-            <h2>Liste <b>Porosish</b></h2>
-        </div>
-        <div class="col-sm-6">
-            <a href="#addProductModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i>
-                <span>Shto te ri</span></a>
+    <div class="table-title">
+        <div class="row">
+            <div class="col-sm-6">
+                <h2>Liste <b>Porosish</b></h2>
+            </div>
+            <div class="col-sm-6">
+                <a href="#addProductModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i>
+                    <span>Shto te ri</span></a>
 
+            </div>
         </div>
     </div>
-</div>
-<table class="table table-striped table-hover table-responsive">
-    <thead>
-        <tr>
-            <th>Order_ID</th>
-            <th>Kamarier</th>
-            <th>Tavolina</th>
-            <th>Koha e krijimit</th>
-            <th>Shiko Detaje</th>
-        </tr>
-    </thead>
-    <tbody>
-        @php
-           $orders = App\Order::orderBy('id','desc')->paginate(9);
-           
-           $useradmin =App\User::find(1)->name;
-        @endphp
+    <div class="table-responsive">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Order_ID</th>
+                    <th>Kamarier</th>
+                    <th>Tavolina</th>
+                    <th>Koha e krijimit</th>
+                    <th class="text-center">Detaje</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                $orders = App\Order::orderBy('id','desc')->paginate(9);
 
-        @foreach ($orders as $order)
-        @php 
-           App\User::find($order->user_id)?$userfature=App\User::find($order->user_id)->name:$userfature="*admin* ne mungese";
-        @endphp
-            <tr>
-                <td>{{$order->id}}</td>
-                <td>{{$userfature}}</td>
-                <td>{{$order->T_id}}</td>
-                <td>{{isset($order->created_at)?$order->created_at->format('H:i:s d/m/Y'):"no date"}}</td>
-                {{-- <td><a href="/orders/{{$order->id}}">Shiko Faturen {{$order->id}}</a></td> --}}
-                <td class="text-right">
-                    <a href="/orders/{{$order->id}}">
-                                <i class="material-icons edit" data-toggle="tooltip" title="Shiko Faturen">&#xE254;</i>
-                    </a>
-            </td>
-            </tr>
+                $useradmin =App\User::find(1)->name;
+                @endphp
 
-        @endforeach
+                @foreach ($orders as $order)
+                @php
+                App\User::find($order->user_id)?$userfature=App\User::find($order->user_id)->name:$userfature="*admin*
+                ne
+                mungese";
+                @endphp
+                <tr>
+                    <td>{{$order->id}}</td>
+                    <td>{{$userfature}}</td>
+                    <td>{{$order->T_id}}</td>
+                    <td>{{isset($order->created_at)?$order->created_at->format('H:i:s d/m/Y'):"no date"}}</td>
+                    {{-- <td><a href="/orders/{{$order->id}}">Shiko Faturen {{$order->id}}</a></td> --}}
+                    <td class="text-center">
+                        <a href="/orders/{{$order->id}}">
+                            <i class="fa fa-file edit" data-toggle="tooltip" title="Shiko Faturen"></i>
+                        </a>
+                    </td>
+                </tr>
 
-        {{$orders->links()}}
-    </tbody>
-</table>
+                @endforeach
+
+                {{$orders->links()}}
+            </tbody>
+        </table>
+    </div>
 </div>
 
 @endrole
