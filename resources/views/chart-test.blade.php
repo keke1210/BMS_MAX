07ccc9bd7c20ba85da517a95702091c5cdd78be3
unknown <klajdi.kume@fshnstudent.info>
2019-02-25 12:34:59 +0100

    tabela

---
+++ resources/views/chart-test.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+  
+  <canvas id="graph" width="400" height="400"></canvas>
+  
+
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+<script>
+    var data=
+    var context=document.querySelector(#)
+    </script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/chart', function () {
+    return view('chart-test');
+});
 
 Route::get('/', function () {
     return view('welcome');
