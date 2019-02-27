2d30a10574e879dafbd54396ce05df08aa72a28f
MarioKapllani <32299334+MarioKapllani@users.noreply.github.com>
2019-02-28 00:41:21 +0100

    new

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="{{asset('/css/fature-style.css')}}">
 @section('content')
     <div class="wrapper wrapper-content ng-scope" id="DivIdToPrint" style="">
-    <div class="col-xs-12 col-md-6">
+    <div class="col-xs-12">
         <a href="/orders" class="btn btn-primary">Anulo order</a> <br><br>
         <form method="GET" action="/orders/create/{{$tavolina->id}}">
             @csrf
@@ -21,15 +21,15 @@
             @if($nrProduktesh>0)
             @php $count=1; @endphp
             @foreach($products as $key=>$product)
-                @if ($count%2==1)
+                @if ($count%3==1)
             
             <div class="col-sm-12 produktet my_radio_box">
                 @endif
-                <div class="form-group col-md-6 text-center">
+                <div class="form-group col-md-4 text-center">
                      <li class="btn btn-lg btn-block btn-huge"><input type="radio" id="{{$product->prod_id}}" name="products" value="{{$product->prod_id}}" data-pem='{{$product->name}}'/><label class="btn btn-danger btn-lg btn-block btn-huge" for="{{$product->prod_id}}">{{$product->name}}</label></li>
                     {{-- <input type="radio" id="product" name="products" value="{{$product->prod_id}}"> --}} 
                 </div>
-                @if($count%2==0)
+                @if($count%3==0)
             </div>
             @endif
         
@@ -64,70 +64,7 @@
             <input type="hidden" name="vlerat" value="{{json_encode($existing)}}" />
         </form>
     </div>
-    <div class="col-xs-12 col-md-6">
-        <div id="DivIdToPrint">
-            <div class="invoice" >
-                    
-            <h4>Fatura ka </h4> <br>
-                <header>
-                  <section>
-                    <h1>Faturë</h1>
-                    <span></span>
-                  </section>
-            
-                  <section>
-                    <span></span>
-                  </section>
-                </header>
-            
-                <main>
-                  <section>
-                    <span>Nr</span>
-                    <span>Proukti</span>
-                    <span>Cmimi </span>
-                    <span>Sasia</span>
-                    <span>TVSH</span>
-                    <span>Nentotal</span>
-                  </section>
-                 
-                  <section>
-                        @php 
-                        // $tvsh=17;
-                        // $cmimi=$orders[$i]->product->cmimi; 
-                        // $sasia=$orders[$i]->sasia;
-                        // $cmimiTvsh= $cmimi+(($cmimi*$tvsh)/100);
-                        // $cmimi_TVSH=number_format((float)$cmimiTvsh,2,'.','');
-                        // $shuma=number_format((float)($sasia * $cmimiTvsh), 2,'.','');
-                    @endphp
-                    <figure>
-                      {{-- <span>{{$i + 1}}</span>
-                      <span><strong>{{$orders[$i]->product->name}}</strong></span>
-                      <span>{{$cmimi}} Lek</span>
-                      <span>{{$cmimi_TVSH}} Lek</span>
-                      <span>{{$sasia}}</span>
-                      <span>{{$tvsh}}%</span>
-                      <span>{{$shuma}} Lek</span> --}}
-                    </figure>
-            
-                    @php
-                    // $array[$i]= $shuma;
-                     @endphp
-                  </section>
-                  @php 
-                //   $totali=array_sum($array);
-                //   $totali = number_format((float)$totali,2,'.','');
-              @endphp
-                  <section>
-                    <span>Total</span>
-                    {{-- <span>{{$totali}} lek</span> --}}
-                  </section>
-                </main>
-                <footer></footer>
-                <footer></footer>
-               
-              </div>
-            </div>
-    </div>
+   
         @else
             <h1>No Products to make a order</h1>
         @endif
