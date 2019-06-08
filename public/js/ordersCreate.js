4f8c3339eae79e4f009e04e95d27f3fd043cd919
MarioKapllani <32299334+MarioKapllani@users.noreply.github.com>
2019-06-08 20:48:36 +0200

    funksioni

--- public/js/ordersCreate.js
+++ public/js/ordersCreate.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function ($) {
-$('#payment').on('click', function(){
-    var array = [];
+    $('#payment').on('click', function () {
+        var array = [];
 
-$('.product-bar').each(function(){
-    var id=$(this).data('p-id');
-    var sasia=$(this).children().find(".pr-quantity").val();
+        $('.product-bar').each(function () {
+            var id = $(this).data('p-id');
+            var sasia = $(this).children().find(".pr-quantity").val();
 
-    array.push({"prod_id":id, "sasia": sasia});
-}); 
-
-console.log(array);
-
-});
-    
+            array.push({
+                "prod_id": id,
+                "sasia": sasia
+            });
+        });
+        
+    });
 });
