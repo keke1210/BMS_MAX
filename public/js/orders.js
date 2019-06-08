e33c34e95be9548fd3f7c0d6534e0cdc8d0eba2c
MarioKapllani <32299334+MarioKapllani@users.noreply.github.com>
2019-06-08 02:37:32 +0200

    u rregullua

--- public/js/orders.js
+++ public/js/orders.js
@@ -2,15 +2,12 @@ jQuery(document).ready(function ($) {
 
     function gjejProdukt($prod_id, $x){
         $i=0;
-        //kontrollon ne te djathte dhe kt
+        //kontrollon ne te djathte dhe kne te majte
         for($i=0;$i<$x;$i++)
         {
-            console.log("Perpara ifit:" +$i);
             if($prod_id==$($('.product-bar')[$i]).data('p-id')){
-                console.log("true");
                 return $i+1;
             }
-            console.log("Pas ifit:" +$i);
         }
         return 0;
     }
@@ -21,27 +18,29 @@ jQuery(document).ready(function ($) {
         $name=$(this).data('emer');
         $cmimi=$(this).data('cmimi');
         $sasia=$(this).data('sasia');
-        console.log("Produkti: "+$id+" \n"+$name+" \n"+$cmimi+" \n"+$sasia);
+
         //Numri i details qe jane ne te djathte
          $nr_details=$(".product-bar").length;
-         console.log("Numri i details "+$nr_details);
+
          //id e produktit dhe numri i details dergohen si parametra tek funksioni
          $pozicioni=gjejProdukt($id,$nr_details);
-         console.log('Vendi '+$pozicioni);
-            if($pozicioni==0)
+
+         if($pozicioni==0)
             {
                 $( ".products" ).append('<div class="product-bar" id="'+$id+'" data-p-id="'+$id+'"><span>'+$name+'</span><span class="pr-cmim">'+$cmimi+'</span><span><input type="number" class="pr-quantity" value='+$sasia+' min="1"></span><button class="btn delete delete-order-detail order-detail"><i class="material-icons" title="Fshi">&#xE872;</i></button></div>');
             }
             else{
-                console.log('Vendi '+$pozicioni);
                 $sasia=$($(".pr-quantity")[$pozicioni]).val();
-                console.log($sasia);
                 $($(".pr-quantity")[$pozicioni]).val()=$sasia+1;
             }
         
         
         calculateSum();
     });
+
+
+
+    
 
     $('.pr-quantity').on('change', function() {
         calculateSum();
